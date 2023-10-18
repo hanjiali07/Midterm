53cc3e649d661d3f649d9c1fea21cfceed4848d7
Hanjia <hanjia.li02@gmail.com>
2023-10-18 08:41:42 -0400

    added user inputs

---
+++ Midterm.py
@@ -0,0 +1,9 @@
+userName = input("Please enter your name: ")
+print("Hi",userName,",welcome to House Value Calculator:")
+
+print("Select the City")
+print("1 Toronto")
+print("2 Montreal")
+
+selection = input("Enter your selection: ")
+
