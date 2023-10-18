d8e24a76e838c2ead4889ff62bfbd1b9d842c680
Hanjia <hanjia.li02@gmail.com>
2023-10-18 09:17:44 -0400

    added a estimated price function

--- Midterm.py
+++ Midterm.py
@@ -1,3 +1,4 @@
+import random
 userName = input("Please enter your name: ")
 print("Hi",userName,",welcome to House Value Calculator:")
 
@@ -12,14 +13,27 @@ getRooms = int(input("Enter number of rooms:"))
 
 if selection == 1:
     print("You have selected Toronto.")
+   
+    if getArea < 0:
+        print("Area must be greater than 0.")
+        a = int(input("Please enter the area:"))
+        print("Area:", getArea)
+    else:
+        print("Area:",getArea)
+
+    if getRooms < 0 or getRooms > 5:
+        print("Number of rooms has to be between 1 and 5.")
+        b = int(input("Please enter number of rooms:"))
+        print("Number of rooms:",getRooms)
+    else:
+        print("Number of rooms:",getRooms) 
+    def torontoPrice():
+        x = random.randint(350, 650)
+        result = x * 1000
+        rooms = result * getRooms
+        return rooms 
+    print(torontoPrice())
 else:
     print("You have selected Montreal.")
 
-if getArea < 0:
-    print("Area must be greater than 0.")
-    a = int(input("Please enter the area:"))
-
-if getRooms < 0 or getRooms > 5:
-    print("Number of rooms has to be between 1 and 5.")
-    b = int(input("Please enter number of rooms:"))
 
