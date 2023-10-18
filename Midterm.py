a96e20caea8c0775427214b176f00d70f5b2e1af
Hanjia <hanjia.li02@gmail.com>
2023-10-18 09:01:58 -0400

    added if statements

--- Midterm.py
+++ Midterm.py
@@ -5,5 +5,21 @@ print("Select the City")
 print("1 Toronto")
 print("2 Montreal")
 
-selection = input("Enter your selection: ")
+selection = int(input("Enter your selection:"))
+getArea = int(input("Enter the area:"))
+getRooms = int(input("Enter number of rooms:"))
+
+
+if selection == 1:
+    print("You have selected Toronto.")
+else:
+    print("You have selected Montreal.")
+
+if getArea < 0:
+    print("Area must be greater than 0.")
+    a = int(input("Please enter the area:"))
+
+if getRooms < 0 or getRooms > 5:
+    print("Number of rooms has to be between 1 and 5.")
+    b = int(input("Please enter number of rooms:"))
 
